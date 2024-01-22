86760d95e36a41da2d2fa46cf6def955270e8ecc
vanessalayson <vanessalayson25@gmail.com>
2024-01-22 13:59:38 +0800

    Created and made navigation work

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+// Navigation Open/Close Buttons
+const container = document.querySelector(".container");
+
+document.querySelector(".open-navbar-icon").addEventListener("click", () => {
+    container.classList.add("change");
+});
+
+document.querySelector(".close-navbar-icon").addEventListener("click", () => {
+    container.classList.remove("change");
+});
+
+
+
+// Navigation Background Colors
+const colors = ["#6495ed", "#7fffd4", "#ffa07a", "#f08080", "#afeeee"]
+
+let i = 0;
+
+Array.from(document.querySelectorAll(".nav-link")).forEach( item => {
+    item.style.cssText = `background-color: ${colors[i++]}`
+});
+
+
+
 // Tours Section price/back toggle button
 Array.from(document.querySelectorAll(".navigation-button")).forEach(item => {
     item.onclick = () => {
